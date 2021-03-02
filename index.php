7140458a094e750351eae732f01f48c31f17c141
Daniel <danielpellegrini87@gmail.com>
2021-03-02 17:13:33 +0100

    Milestone 1 COMPLETED

--- index.php
+++ index.php
@@ -2,8 +2,39 @@
 
   require_once __DIR__ . '/database/database.php';
 
-  var_dump($albums);
+  // var_dump($albums);
 
 
+?>
 
- ?>
+ <link rel="stylesheet" href="css/style.css">
+
+ <div id="app">
+
+   <header>
+     <div class="container">
+       <img src="img/logo.png" alt="logo" />
+     </div>
+   </header>
+
+   <div class="cds-container container">
+
+     <?php foreach ($albums as $album) {
+
+      ?>
+
+       <div class="cd">
+         <?php $image = $album['poster'] ?>
+         <img src='<?php echo $image ?>' alt="<?php echo $album['title'] ?>">
+         <h3><?php echo $album['title'] ?></h3>
+         <span class="author"><?php echo $album['author'] ?></span>
+         <span class="year"><?php echo $album['year'] ?></span>
+       </div>
+
+       <?php
+
+    } ?>
+
+   </div>
+
+ </div>
---
+++ webpack.mix.js
@@ -0,0 +1,4 @@
+let mix = require('laravel-mix');
+mix
+  .sass('scss/style.scss', 'css/')
+  .options({processCssUrls: false});
