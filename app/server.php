a229e9450f94bbf673712ee89845098db63c60ba
Daniel <danielpellegrini87@gmail.com>
2021-03-03 12:40:38 +0100

    Milestone 2 COMPLETED

--- app/server.php
+++ app/server.php
@@ -1,10 +1,12 @@
 <?php
 
-require_once __DIR__ . '/../database/database.php';
+  require_once __DIR__ . '/../database/database.php';
 
-header('Content-Type: application/json');
+  header('Content-Type: application/json');
 
-echo json_encode($albums);
+  echo json_encode($albums);
 
-
+  
+  
 ?>
+
